bbd2faa77d099af0674befd9bd04d98f91b2e887
Vitaliy Khantimerov <vitalij.khantimerov@privatbank.ua>
2021-11-03 22:33:28 +0200

    hw 6.0.

--- src/com/pb/khantimerov/hw6/Cat.java
+++ src/com/pb/khantimerov/hw6/Cat.java
@@ -1,4 +1,10 @@
 package com.pb.khantimerov.hw6;
 
-public class Cat {
+/**
+ * Dog, Cat, Horse переопределяют методы makeNoise, eat.
+ * Добавьте переменные (поля) в классы Dog, Cat, Horse, характеризующие только этих животных.
+ * В классах Dog, Cat, Horse переопределить методы toString, equals, hashCode.
+ */
+public class Cat extends Animal{
+
 }
