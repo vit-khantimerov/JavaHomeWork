1de35dfa4bc1016a8655992a371ba0dc0aa4ca7f
Vitaliy Khantimerov <vitalij.khantimerov@privatbank.ua>
2021-11-28 17:32:34 +0200

    Commit 29.11.2021

--- src/com/pb/khantimerov/hw10/Main.java
+++ src/com/pb/khantimerov/hw10/Main.java
@@ -3,13 +3,13 @@ package com.pb.khantimerov.hw10;
 public class Main {
     public static void main(String[] args) {
 
-        NumBox<IntNumber> integers = new NumBox<>(4);
-        integers.add( new IntNumber(100.),0);
-        integers.add( new IntNumber(0.),1);
-        //integers.add( new IntNumber(200.),2);
-        integers.add( new IntNumber(-1000.),3);
-        integers.add( new IntNumber(-1.),3);
-        integers.add( new IntNumber(1000.),4);
+        NumBox<Integer> integers = new NumBox<>(4);
+        integers.add( new Integer(100),0);
+        integers.add( new Integer(0),1);
+        //integers.add( new Integer(200),2);
+        integers.add( new Integer(-100),3);
+        integers.add( new Integer(-1),3);
+        integers.add( new Integer(1000),4);
 
         integers.get(2);
         integers.get(3);
@@ -20,16 +20,16 @@ public class Main {
 
         System.out.println();
 
-        NumBox<FloatNumber> floats = new NumBox<>(5);
-        floats.add( new FloatNumber(10.0),0);
-        floats.add( new FloatNumber(500.0),1);
-        //floats.add( new FloatNumber(50.0),2);
-        floats.add( new FloatNumber(-10.0),3);
-        floats.add( new FloatNumber(99.0), 4);
-        floats.add( new FloatNumber(99.0),5);
+        NumBox<Float> floats = new NumBox<>(5);
+        floats.add( new Float(10.0),0);
+        floats.add( new Float(500.0),1);
+        //floats.add( new Float(50.0),2);
+        floats.add( new Float(-10.0),3);
+        floats.add( new Float(99.0), 4);
+        floats.add( new Float(99.0),5);
 
-        floats.get(3);
         floats.get(2);
+        floats.get(3);
         floats.length();
         floats.average();
         floats.sum();
--- src/com/pb/khantimerov/hw10/NumBox.java
+++ src/com/pb/khantimerov/hw10/NumBox.java
@@ -1,17 +1,17 @@
 package com.pb.khantimerov.hw10;
 
-public class NumBox <T extends Number>{
+public class NumBox <T extends java.lang.Number>{
     private final T[] numbers;
 
     @SuppressWarnings("unchecked")
     public NumBox(int size) {
-        numbers = (T[]) new Number[size];
+        numbers = (T[]) new java.lang.Number[size];
     }
 
     public void add(T number, int index) {
         try {
             this.numbers[index] = number;
-            System.out.println("Добавлен " + number.getClass().getSimpleName() + "[" + index + "] = " + this.numbers[index].get());
+            System.out.println("Добавлен " + number.getClass().getSimpleName() + "[" + index + "] = " + this.numbers[index].doubleValue());
         } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println(number.getClass().getSimpleName() + "[" + index + "] не добален. Превышена длина массива.");
         }
@@ -19,7 +19,7 @@ public class NumBox <T extends Number>{
 
     public T get(int index) {
         try {
-            System.out.println(numbers.getClass().getSimpleName() + " [" + index + "] = " + numbers[index].get());
+            System.out.println(numbers.getClass().getSimpleName() + " [" + index + "] = " + numbers[index].doubleValue());
             return numbers[index];
         } catch (NullPointerException e) {
             System.out.println(numbers.getClass().getSimpleName() + " [" + index + "] " + "Не заолнен.");
@@ -48,7 +48,7 @@ public class NumBox <T extends Number>{
                         average = 0.0;
                     }
                     length++;
-                    average += t.get();
+                    average += t.doubleValue();
                 }
             }
             System.out.println("Среднее = " + average / length);
@@ -67,7 +67,7 @@ public class NumBox <T extends Number>{
                 if (sum == null) {
                     sum = 0.0;
                 }
-            sum += t.get();
+            sum += t.doubleValue();
             }
         }
         System.out.println("Сумма = " + sum);
@@ -82,13 +82,13 @@ public class NumBox <T extends Number>{
             x++;
         }
         if (numbers[x] != null) {
-            max = numbers[x].get();
+            max = numbers[x].doubleValue();
         }
 
             for (int i = x; i < numbers.length; i++) {
                 if (numbers[i] != null) {
-                    if (numbers[i].get() > max) {
-                        max = numbers[i].get();
+                    if (numbers[i].doubleValue() > max) {
+                        max = numbers[i].doubleValue();
                         x = i;
                     }
                 }
