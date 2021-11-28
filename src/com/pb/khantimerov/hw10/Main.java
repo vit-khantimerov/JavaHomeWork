e565042965a6d4293667f62d4fee74360c86392c
Vitaliy Khantimerov <vitalij.khantimerov@privatbank.ua>
2021-11-28 17:36:07 +0200

    Commit 29.11.2021

--- src/com/pb/khantimerov/hw10/Main.java
+++ src/com/pb/khantimerov/hw10/Main.java
@@ -4,12 +4,12 @@ public class Main {
     public static void main(String[] args) {
 
         NumBox<Integer> integers = new NumBox<>(4);
-        integers.add( new Integer(100),0);
-        integers.add( new Integer(0),1);
-        //integers.add( new Integer(200),2);
-        integers.add( new Integer(-100),3);
-        integers.add( new Integer(-1),3);
-        integers.add( new Integer(1000),4);
+        integers.add( 100,0);
+        integers.add( 0,1);
+        //integers.add( 200,2);
+        integers.add( -100,3);
+        integers.add( -1,3);
+        integers.add( 1000,4);
 
         integers.get(2);
         integers.get(3);
@@ -21,12 +21,12 @@ public class Main {
         System.out.println();
 
         NumBox<Float> floats = new NumBox<>(5);
-        floats.add( new Float(10.0),0);
-        floats.add( new Float(500.0),1);
-        //floats.add( new Float(50.0),2);
-        floats.add( new Float(-10.0),3);
-        floats.add( new Float(99.0), 4);
-        floats.add( new Float(99.0),5);
+        floats.add( 10.0f,0);
+        floats.add( 500.0f,1);
+        //floats.add(50.0f,2);
+        floats.add(-10.0f,3);
+        floats.add(99.0f, 4);
+        floats.add( 99.0f,5);
 
         floats.get(2);
         floats.get(3);
